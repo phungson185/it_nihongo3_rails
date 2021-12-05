98ef64ab601fbc868b205494cb7196541f631043
phungson185 <phungson185@gmail.com>
2021-12-05 15:14:56 +0000

    commi·

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,10 @@ ActiveRecord::Schema.define(version: 2021_12_05_145540) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
